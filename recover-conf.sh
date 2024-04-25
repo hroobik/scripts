7533341536e5872d4fb338ac9a900293ff12dbd0
Ruben Harutyunyan <ruben.harutyunyan@sceon.am>
2024-04-25 21:57:13 +0400

    recover nginx conf

---
+++ recover-conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set pid of nginx master process here
+pid=3754
+
+# generate gdb commands from the process's memory mappings using awk
+cat /proc/$pid/maps | awk '$6 !~ "^/" {split ($1,addrs,"-"); print "dump memory mem_" addrs[1] " 0x" addrs[1] " 0x" addrs[2] ;}END{print "quit"}' > gdb-commands
+
+# use gdb with the -x option to dump these memory regions to mem_* files
+gdb -p $pid -x gdb-commands
+
+# look for some (any) nginx.conf text
+#grep worker_connections mem_*
+#grep server_name mem_*
+
